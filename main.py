23abef29437d6dbd01d220e05e66c03ed005799b
MRX <155355976+Finexeagle@users.noreply.github.com>
2025-07-19 23:29:10 +0530

    Created UPDATE, DELETE, POST GET functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
@@ -29,8 +30,8 @@ async def root():
     return {"message": "Hello, World!"}
 
 @app.get("/posts")
-# def get_posts():
-#     return {"data": my_posts}
+def get_posts():
+     return {"data": my_posts}
 
 def find_post(id: int):
     for post in my_posts:
@@ -38,7 +39,14 @@ def find_post(id: int):
             return{"post": post}
     return None
 
-@app.post("/posts")
+def get_post_index(id: int):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+        
+    return None
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000)
@@ -46,12 +54,26 @@ def create_post(post: Post):
     return {"data": post_dict}
 
 
+@app.get("/post/latest")
+def get_latest_posts():
+    post_dict = my_posts[-1]
+    return {"data": post_dict}
+
+
 @app.get("/post/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(id)
-    if post:
-        return post
-    return {"error": "Post not found"}  
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return {"data": post}
 
 
-
+@app.delete("/post/{id}")
+def delete_post(id: int):
+    post_index = get_post_index(id)
+    if post_index is not None:
+        my_posts.pop(post_index)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=404, detail="Post with id doesent Exist")
+    
