780913c60a7e0db09aba1a32fed26ff5eae52d48
MRX <155355976+Finexeagle@users.noreply.github.com>
2025-07-19 18:22:04 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from fastapi import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
+
+app = FastAPI()
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "title of post 2", "content": "content of post 2", "id": 2}]
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "title": "Sample Post Title",
+    #             "content": "This is the content of the sample post."
+    #         }
+    #     }
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+@app.get("/posts")
+# def get_posts():
+#     return {"data": my_posts}
+
+def find_post(id: int):
+    for post in my_posts:
+        if post['id'] == id:
+            return{"post": post}
+    return None
+
+@app.post("/posts")
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
